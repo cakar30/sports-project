1b982d64f70b7d996502b5f8a6ee1e4fa46a0ff5
CemAkr <107773674+cakar30@users.noreply.github.com>
2022-11-20 02:26:20 +0300

    first commit sport project

---
+++ src/main/java/com/springjssports/sportsproject/SportsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springjssports.sportsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsProjectApplication.class, args);
+    }
+
+}
