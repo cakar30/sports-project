6e238246ce6b6cd30763593b9adf351895fb5d8c
CemAkr <107773674+cakar30@users.noreply.github.com>
2022-11-20 10:54:38 +0300

    postgresssss

---
+++ src/main/java/com/springjssports/sportsproject/controller/testController.java
@@ -0,0 +1,18 @@
+package com.springjssports.sportsproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/user")
+public class testController {
+
+
+
+@GetMapping("/testMethod")
+public String getTestMethod(){
+    return "Hello Cem";
+}
+
+}
