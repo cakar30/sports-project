dbdd87ee102f1bfcddb3d9b5050044cc40b79e50
CemAkr <107773674+cakar30@users.noreply.github.com>
2022-11-20 10:28:42 +0300

    postgres

--- src/main/java/com/springjssports/sportsproject/SportsProjectApplication.java
+++ src/main/java/com/springjssports/sportsproject/SportsProjectApplication.java
@@ -2,11 +2,13 @@ package com.springjssports.sportsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class SportsProjectApplication {
 
     public static void main(String[] args) {
+     //   System.out.println("Spring Core Version:- " + SpringVersion.getVersion());
         SpringApplication.run(SportsProjectApplication.class, args);
     }
 
